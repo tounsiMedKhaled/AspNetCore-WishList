e693a85b287260d08136c15821714d0b9a46ada3
Mohamed Khaled TOUNSI <tounsi.medkhaled@gmail.com>
2018-06-08 11:51:39 +0100

    fix unit test

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+  
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(i => i.Id == id);
+            if(item ==  null)
+            {
+                return NotFound();
+            }
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
