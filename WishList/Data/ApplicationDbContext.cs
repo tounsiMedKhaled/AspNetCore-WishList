4670990c8fcd5fbc95f9c573f6a7be2ce5c92c4b
Mohamed Khaled TOUNSI <tounsi.medkhaled@gmail.com>
2018-06-08 10:38:31 +0100

    Adding Items to DBcontext

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
@@ -12,5 +13,7 @@ namespace WishList.Data
         {
 
         }
+
+        public DbSet<Item> Items { get; set; }
     }
 }
